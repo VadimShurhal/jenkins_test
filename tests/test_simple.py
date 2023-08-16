b04b1fcd13537779f092ced5b69ef8c09f21f85c
VadymShurkhal <vadimshurhal@gmail.com>
2023-08-16 20:50:27 +0300

    add some test and new pipeline

---
+++ tests/test_simple.py
@@ -0,0 +1,17 @@
+import os
+
+import pytest
+
+TEST_UPPER = os.environ.get('TEST_UPPER')
+TEST_ISUPPER = os.environ.get('TEST_ISUPPER')
+
+
+class TestClass:
+
+    @pytest.mark.skipif(TEST_UPPER, reason='Bug 543534')
+    def test_upper(self):
+        assert 'foo'.upper() == 'FOO'
+
+    @pytest.mark.skipif(TEST_ISUPPER, reason='Bug 42342')
+    def test_isupper(self):
+        assert 'FOO'.isupper()
