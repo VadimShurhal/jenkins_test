b1e44d583f40d090f52e9ebce9136a20da172680
VadymShurkhal <vadimshurhal@gmail.com>
2023-08-17 00:00:03 +0300

    change env variable in test

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,17 +1,18 @@
+import distutils
 import os
 
 import pytest
 
-TEST_UPPER = os.environ.get('TEST_UPPER')
-TEST_ISUPPER = os.environ.get('TEST_ISUPPER')
+TEST_UPPER = bool(eval(os.environ.get('TEST_UPPER')))
+TEST_ISUPPER = bool(eval(os.environ.get('TEST_ISUPPER')))
 
 
 class TestClass:
 
-    @pytest.mark.skipif(bool(TEST_UPPER), reason='Bug 543534')
+    @pytest.mark.skipif(TEST_UPPER, reason='Bug 543534')
     def test_upper(self):
         assert 'foo'.upper() == 'FOO'
 
-    @pytest.mark.skipif(bool(TEST_ISUPPER), reason='Bug 42342')
+    @pytest.mark.skipif(TEST_ISUPPER, reason='Bug 42342')
     def test_isupper(self):
         assert 'FOO'.isupper()
