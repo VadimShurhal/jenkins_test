62d3235f948ea70f285c034000879b07fd2cdab1
VadymShurkhal <vadimshurhal@gmail.com>
2023-08-16 23:48:28 +0300

    fix skip params

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -8,10 +8,10 @@ TEST_ISUPPER = os.environ.get('TEST_ISUPPER')
 
 class TestClass:
 
-    @pytest.mark.skipif(TEST_UPPER, reason='Bug 543534')
+    @pytest.mark.skipif(bool(TEST_UPPER), reason='Bug 543534')
     def test_upper(self):
         assert 'foo'.upper() == 'FOO'
 
-    @pytest.mark.skipif(TEST_ISUPPER, reason='Bug 42342')
+    @pytest.mark.skipif(bool(TEST_ISUPPER), reason='Bug 42342')
     def test_isupper(self):
         assert 'FOO'.isupper()
